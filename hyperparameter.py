101897d4372c579f2dc7c0d0185b9551d0895fd7
Zachariah K. Sharma <33808784+zachariahsharma@users.noreply.github.com>
2023-11-22 14:31:02 -0600

    Update hyperparameter.py

--- hyperparameter.py
+++ hyperparameter.py
@@ -3,5 +3,6 @@ from ultralytics import YOLO
 # model.tune(data='./Robot-Locator-7/data.yaml', epochs=20, iterations=100,
 #            optimizer='AdamW', plots=False, save=False, val=False, device=0, resume=True)
 new_model = YOLO('/RobotLocator/runs/detect/tune2/weights/best.pt')
-new_model.train(epochs=200, device=0)
+new_model.train("/RobotLocator/Robot-Locator-7/data.yaml",
+                epochs=200, device=0)
 new_model.export("/RobotLocatoer.pt")
