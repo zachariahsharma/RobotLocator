4a69b023e3caddf41b76c51d03e9d68817119f32
Zachariah K. Sharma <33808784+zachariahsharma@users.noreply.github.com>
2023-11-21 19:42:43 -0600

    updates

--- main.sh
+++ main.sh
@@ -2,4 +2,6 @@
 git clone https://github.com/zachariahsharma/RobotLocator.git
 cd RobotLocator
 pip install -r "requirements.txt"
+curl -d '{"finished":"false"}' -H 'localtonet-skip-warning: True' -H 'Content-Type: application/json' 6n90ghq.localto.net
 python3 hyperparameter.py
+curl -d '{"finished":"true"}' -H 'localtonet-skip-warning: True' -H 'Content-Type: application/json' 6n90ghq.localto.net
