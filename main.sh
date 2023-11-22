75a13ae80669243295846824c8901dfb61822c72
Zachariah K. Sharma <33808784+zachariahsharma@users.noreply.github.com>
2023-11-21 21:17:54 -0600

    ASDF

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # git clone https://github.com/zachariahsharma/RobotLocator.git
-cd RobotLocator
+# cd RobotLocator
 # pip install -r "requirements.txt"
 # curl -d '{"finished":"false"}' -H 'localtonet-skip-warning: True' -H 'Content-Type: application/json' 6n90ghq.localto.net
 python3 hyperparameter.py
