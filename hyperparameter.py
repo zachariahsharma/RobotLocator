467351bb48044bbb2ff21d29cd1aa677d355229a
Zachariah K. Sharma <33808784+zachariahsharma@users.noreply.github.com>
2023-11-21 19:50:40 -0600

    cuda

--- hyperparameter.py
+++ hyperparameter.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 model = YOLO('yolov8n.pt')
 model.tune(data='./Robot-Locator-7/data.yaml', epochs=30, iterations=300,
-           optimizer='AdamW', plots=False, save=False, val=False)
+           optimizer='AdamW', plots=False, save=False, val=False, device=0)
 new_model = YOLO('./runs/tune/weights/best.pt')
-new_model.train(epochs=200)
+new_model.train(epochs=200, device=0)
 new_model.export("./RobotLocatoer.pt")
