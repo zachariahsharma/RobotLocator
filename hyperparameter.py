ba01023722314e776e2db5107ba69c2da865e512
Zachariah K. Sharma <33808784+zachariahsharma@users.noreply.github.com>
2023-11-22 14:19:21 -0600

    asdf

--- hyperparameter.py
+++ hyperparameter.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
-model = YOLO('yolov8n.pt')
-model.tune(data='./Robot-Locator-7/data.yaml', epochs=20, iterations=100,
-           optimizer='AdamW', plots=False, save=False, val=False, device=0, resume=True)
-new_model = YOLO('./runs/tune/weights/best.pt')
+# model = YOLO('yolov8n.pt')
+# model.tune(data='./Robot-Locator-7/data.yaml', epochs=20, iterations=100,
+#            optimizer='AdamW', plots=False, save=False, val=False, device=0, resume=True)
+new_model = YOLO('/RobotLocator/runs/tune/weights/best.pt')
 new_model.train(epochs=200, device=0)
-new_model.export("./RobotLocatoer.pt")
+new_model.export("/RobotLocatoer.pt")
