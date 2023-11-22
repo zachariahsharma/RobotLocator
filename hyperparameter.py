038d6039bea72eef639a496bfe327e9f0765f013
Zachariah K. Sharma <33808784+zachariahsharma@users.noreply.github.com>
2023-11-22 14:49:01 -0600

    Update hyperparameter.py

--- hyperparameter.py
+++ hyperparameter.py
@@ -6,4 +6,4 @@ new_model = YOLO('/RobotLocator/runs/detect/tune2/weights/best.pt')
 new_model.train("/RobotLocator/Robot-Locator-7/data.yaml",
                 epochs=200, device=0)
 new_model.export()
-/lr0=0.0055, lrf=0.01522, momentum=0.82781, weight_decay=0.00034, warmup_epochs=2.78171, warmup_momentum=0.95, box=5.53366, cls=0.57088, dfl=1.49239,
+# lr0=0.0055, lrf=0.01522, momentum=0.82781, weight_decay=0.00034, warmup_epochs=2.78171, warmup_momentum=0.95, box=5.53366, cls=0.57088, dfl=1.49239,
